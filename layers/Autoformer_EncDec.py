85df1b6d82a25e1f9a622ffff8d557e8ea973acc
StephenHB <Stephen.zhang22@gmail.com>
2025-01-14 13:26:11 -0500

    add customized layernorm for seasonality using torch

---
+++ layers/Autoformer_EncDec.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class my_Layernorm(nn.Module):
+    """
+    Customized layernorm for seasonality
+    """
+    def __init__(self, channels):
+        super(my_Layernorm, self).__init__()
+        self.layernorm = nn.LayerNorm(channels)
+
+    def forward(self, x):
+        x_hat = self.layernorm(x)
+        bias = torch.mean(x_hat, dim=1).unsqueeze(1).repeat(1, x.shape[1], 1)
+        return x_hat - bias
